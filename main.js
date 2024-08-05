f4acdd8a5d620b1b7fcef1a47dd85a018a104e1c
Vikkywoofyz <155740202+Vikkywoofyz@users.noreply.github.com>
2024-08-05 17:37:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed (".text",{
+    strings:["Data Analyst","SQL Developer","Database developer","Data science passionate","Animals Enthusiast"],
+    typespeed:100,
+    backspeed:100,
+    backdelay:1000,
+    loop:true
+});
